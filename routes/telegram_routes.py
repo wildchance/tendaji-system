3428e2d4489c25076aab7416979fe6820baba4f0
wildchance_conglomerate <misangoedgaruche@gmail.com>
2025-11-21 14:13:24 +0300

    Update telegram_routes.py

--- routes/telegram_routes.py
+++ routes/telegram_routes.py
@@ -1,5 +1,23 @@
-from decouple import config
+from fastapi import APIRouter
+from pydantic import BaseModel
 import requests
+from decouple import config
+
+router = APIRouter()
 
 BOT_TOKEN = config("TELEGRAM_BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
+class Alert(BaseModel):
+    chat_id: str
+    message: str
+
+@router.post("/telegram/send-message")
+def send_message(alert: Alert):
+    url = f"{BASE_URL}/sendMessage"
+    payload = {
+        "chat_id": alert.chat_id,
+        "text": alert.message
+    }
+    response = requests.post(url, json=payload)
+    return response.json()
