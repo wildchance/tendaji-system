68e7243478ef3668dc471af4dcecc138a67cb319
wildchance <misangoedgaruche@gmail.com>
2025-11-21 11:17:06 +0300

    Update main.py

--- trading-engine/main.py
+++ trading-engine/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
-from routes.signals import router as signals_router
-from routes.trade import router as trade_router
-from routes.webhook import router as webhook_router
+from core.signals import router as signals_router
+from core.trade import router as trade_router
+from core.webhook import router as webhook_router
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "🚀 FastAPI Trading Engine Running on Railway!"}
+    return {"message": "Tendaji Trading Engine is running 🚀"}
 
-# Include Routes
 app.include_router(signals_router)
 app.include_router(trade_router)
 app.include_router(webhook_router)
