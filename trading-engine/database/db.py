0b67c46a3ec0d68c172a05cbdeeeb52ade63c01f
wildchance <misangoedgaruche@gmail.com>
2025-11-21 11:28:45 +0300

    Update db.py

--- trading-engine/database/db.py
+++ trading-engine/database/db.py
@@ -1,13 +1,17 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/trading"
+DATABASE_URL = "postgresql+asyncpg://<username>:<password>@<host>/<dbname>"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
 
 async def get_db():
     async with SessionLocal() as session:
         yield session
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
