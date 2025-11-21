313b931b9327abd39daeb8befa903209077cc0a2
wildchance_conglomerate <misangoedgaruche@gmail.com>
2025-11-21 15:58:51 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
