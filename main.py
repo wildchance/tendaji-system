59bf9812ac45884fcadde5dd43a11e1d29eb1c48
wildchance_conglomerate <misangoedgaruche@gmail.com>
2025-11-22 20:02:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-
-# Import routers
 from core.signals import router as signals_router
 from core.trade import router as trade_router
 from core.webhook import router as webhook_router
@@ -8,6 +6,8 @@ from database.db import init_db
 from routes.telegram_routes import router as telegram_router
 from routes.admin import router as admin_router
 from routes.market import router as market_router 
+
+app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
@@ -22,4 +22,9 @@ app.include_router(trade_router)
 app.include_router(webhook_router)
 app.include_router(telegram_router)
 app.include_router(admin_router)
-app.include_router(market_router)  
+app.include_router(market_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "Tendaji system API is live"}
