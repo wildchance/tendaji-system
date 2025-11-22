645ceb89126cce3ad6d22a00009d682842853b9a
wildchance_conglomerate <misangoedgaruche@gmail.com>
2025-11-22 22:54:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from core.signals import router as signals_router
 from core.trade import router as trade_router
 from core.webhook import router as webhook_router
 from database.db import init_db
 from routes.telegram_routes import router as telegram_router
 from routes.admin import router as admin_router
+from routes.market import router as market_router  
 
-from routes.market import router as market_router
-app.include_router(market_router)
-
-app = FastAPI(title="Tendaji Trading Engine")
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = FastAPI()  
 
 @app.on_event("startup")
 async def startup_event():
@@ -21,10 +16,6 @@ async def startup_event():
 @app.get("/")
 def home():
     return {"message": "Tendaji system API is live"}
-
-@app.post("/webhook/signal")
-async def trading_signal(payload: dict):
-    return {"received": payload, "status": "success"}
 
 app.include_router(signals_router)
 app.include_router(trade_router)
