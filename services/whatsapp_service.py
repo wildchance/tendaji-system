930d5789fed16e301d114db6631518b25f66418e
wildchance_conglomerate <misangoedgaruche@gmail.com>
2025-11-23 13:45:23 +0300

    Update whatsapp_service.py

--- services/whatsapp_service.py
+++ services/whatsapp_service.py
@@ -1,1 +1,10 @@
+import requests
+import os
 
+CALLMEBOT_API = "https://api.callmebot.com/whatsapp.php"
+WHATSAPP_PHONE = os.getenv("WHATSAPP_PHONE")
+WHATSAPP_API_KEY = os.getenv("WHATSAPP_API_KEY")
+
+def send_whatsapp_message(message):
+    url = f"{CALLMEBOT_API}?phone={WHATSAPP_PHONE}&text={message}&apikey={WHATSAPP_API_KEY}"
+    requests.get(url)
