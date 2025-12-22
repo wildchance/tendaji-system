68974af2fb2d287ab74c5ef8f0f817fbd49ade87
wildchance_conglomerate <misangoedgaruche@gmail.com>
2025-12-22 13:24:18 +0300

    Update bot.py

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,5 +1,10 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from dotenv import load_dotenv
+load_dotenv()
+
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
